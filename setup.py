6a6f406a879f07c40a3016695a2c98c4054bab83
perrystallings <perry.stallings@synapsefi.com>
2023-10-30 10:01:31 -0500

    fixing version

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/perrystallings/parrot_api_core',
-    version='0.1.13',
+    version='0.1.14',
     zip_safe=False
 )
